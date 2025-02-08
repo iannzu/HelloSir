2bb38de341556719b4369542031303b32dc92a04
iannzu <cbiancaysabel@gmail.com>
2025-02-08 14:17:59 +0800

    introduce widgets

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+void main() {
+  print("I am a mobile developer");
+  print("Hi my name is Bianca");
+  var name = "Bianca Ysabel";
+  print("Hello $name");
+  const place = "SFC";
+  final color = "mlue";
+  //type annotation
+
+  String vegetable = "ngamage";
+
+  int? age;
+
+  print(age);
+
+}
